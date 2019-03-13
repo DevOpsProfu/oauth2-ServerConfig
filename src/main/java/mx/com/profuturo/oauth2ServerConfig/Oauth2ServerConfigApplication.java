d666930e58aaebdd5851b6f9d65d3e9a91a30557
DevOpsProfu <la501857@MacBook-Pro-de-Luis.local>
2019-03-13 10:32:27 -0600

    Carga proyecto base

---
+++ src/main/java/mx/com/profuturo/oauth2ServerConfig/Oauth2ServerConfigApplication.java
@@ -0,0 +1,13 @@
+package mx.com.profuturo.oauth2ServerConfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oauth2ServerConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Oauth2ServerConfigApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mx/com/profuturo/oauth2ServerConfig/Oauth2ServerConfigApplicationTests.java
@@ -0,0 +1,16 @@
+package mx.com.profuturo.oauth2ServerConfig;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Oauth2ServerConfigApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
